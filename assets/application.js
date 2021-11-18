16614ecb70e0911ba3fd842ea9fbb88d2eea74b6
Enz000 <enzosantamaria@outlook.fr>
2021-11-18 11:24:14 +0100

    Update style:hover

--- assets/application.js
+++ assets/application.js
@@ -1,7 +1,12 @@
+const plus = document.getElementById('input_plus')
+const minus = document.getElementById('input_minus')
+const inputQuantity = document.getElementById('quantity')
+
 window.addEventListener("DOMContentLoaded", () => {
   const nav = document.querySelectorAll("[id=nav_list]");
   const menu = document.querySelector("#display_menu");
-  console.log(nav);
+
+
   nav.forEach((el) => {
     el.addEventListener("mouseenter", (e) => {
       const target = e.target.innerText;
@@ -9,10 +14,33 @@ window.addEventListener("DOMContentLoaded", () => {
         menu.classList.remove("hidden");
       }
     });
-    menu.addEventListener("mouseleave", (e) => {
-      const target = e.target.innerText;
-
-      menu.classList.add("hidden");
+    menu.addEventListener("mouseleave", () => {
+     menu.classList.add("hidden");
     });
   });
+  
+
+
 });
+
+
+
+const openMenu = () => {
+  document.getElementById("mobile-menu").classList.remove("hidden");
+};
+const closeMenu = () => {
+  document.getElementById("mobile-menu").classList.add("hidden");
+};
+
+
+
+plus.addEventListener('click', (e) => {
+  e.preventDefault();
+  inputQuantity.stepUp()
+  
+})
+minus.addEventListener('click', (e) => {
+  e.preventDefault();
+  inputQuantity.stepDown()
+  
+})
